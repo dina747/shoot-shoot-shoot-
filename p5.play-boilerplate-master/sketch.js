b108e72a50022acb77c4677f82c5bcff00aaa2de
dina747 <69771997+dina747@users.noreply.github.com>
2020-09-14 21:39:02 -0400

    Update sketch.js

--- p5.play-boilerplate-master/sketch.js
+++ p5.play-boilerplate-master/sketch.js
@@ -1,11 +1,5 @@
 var car1;
 var speed,weight;
-var two ;
-var three ;
-var four ;
-var wall2 ;
-var wall3 ;
-var wall4 ;
 
 
 function setup() {
@@ -13,42 +7,46 @@ function setup() {
  
  
   wall1 = createSprite(1060,45,20,30);
-  wall2 = createSprite(1060,130,20,30);
-  wall3 = createSprite(1060,235,20,30);
-  wall4 = createSprite(1060,350,20,30);
+
+  
 
  car1 = createSprite(1041,45,20,10);
 
  
 car1.velocityX =speed;
 
-
 speed=random(55,90)
 weight=random(400,1500)
 
-if(wall1.x-car1.x < (car1.width+wall1.width)/2)
-{
-  car1.velocityX=0;
-  var deformation=0.5 * weight * speed* speed/22509;
-  if(deformation>180)
-{
-  car1.shapeColor=color(255,0,0);
-}
-if(deformation<180 && deformation>100)
-{
-  car1.shapeColor=color(96,255,128);
-}
-if(deformation<100) 
-{
-  car1.shapeColor=color(80,208,255);
- } 
 
-}
+
+  
 
 }
 function draw() {
 
 background("black");
+  
+  
+  if(wall1.x-car1.x < (car1.width+wall1.width)/2){
+  car1.velocityX=0;
+  var deformation=0.5 * weight * speed* speed/22509;
+  if(deformation>180){
+    
+  car1.shapeColor=color(255,0,0);
+    
+}
+if(deformation>100 && deformation<180){
+  
+  car1.shapeColor=color(96,255,128);
+}
+if(deformation<100) {
+  
+  car1.shapeColor=color(80,208,255);
+  
+ } 
+
+}
 
 drawSprites();
 }
